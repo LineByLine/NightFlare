45cb206755a8061727d81c8a666e06e6bb359040
Christian <c.t.segumalian@gmail.com>
2018-11-07 13:43:53 -0800

    Player can fire bullets with playerShootingProjectile
    Click RMB to fire. Need to add reload, unlimited ammo, hold down to fire, cooldown.

---
+++ 2DTopDownShooter/Assets/scripts/BulletBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour {
+	private SpriteRenderer sr;
+	private Rigidbody2D rb;
+	public float speed;
+	private enemyBehavior damaging; //the enemy script to access variables
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Locked and loaded");
+		sr = gameObject.GetComponent<SpriteRenderer>();
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		gameObject.transform.rotation = GameObject.FindGameObjectsWithTag("Player")[0].transform.rotation;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//WHY DOES THIS WORK
+		//I couldn't figure out how to add the correct velocity, so I added the current position to a vector
+		//Why does this vector make it go in the direction I want?
+		rb.MovePosition(Vector2.MoveTowards(transform.position, transform.position + new Vector3(speed * Time.deltaTime, 0.0f, 0.0f), speed * Time.deltaTime));
+		gameObject.transform.Translate(new Vector2(speed * Time.deltaTime, 0.0f));
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		string tag = collision.gameObject.tag;
+		if (tag != "Player")
+		{
+			if (tag == "Enemy")
+			{
+				damaging = collision.gameObject.GetComponent<enemyBehavior>();
+				damaging.currentHealth--;
+			}
+			//Kill this object if it hits anything that isn't a Player
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ 2DTopDownShooter/Assets/scripts/playerShootingProjectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShootingProjectile : MonoBehaviour {
+	public GameObject bulletObject;
+	[Header("Shooting Number Variables")]
+	public Transform firingPoint; //Where the bullet will travel/check from (realistic gun)
+	private weaponInfo useClip; //the weapon info script to access variables
+
+	// Use this for initialization
+	void Start () {
+
+        useClip = firingPoint.GetComponent<weaponInfo>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			fire();
+		}
+	}
+
+	void fire()
+	{
+		//if your current ammo is empty, we try to reload
+		if (useClip.currentClip == 0)
+		{
+			useClip.reload();
+		}
+		else //you can fire if u have ammo in your clip
+		{		
+			//Spawn bullet
+			GameObject.Instantiate(bulletObject, gameObject.transform.position,  gameObject.transform.rotation);
+			
+
+			//fires one bullet, subtracting from the current ammo 
+			useClip.currentClip--;
+		}
+	}
+}
