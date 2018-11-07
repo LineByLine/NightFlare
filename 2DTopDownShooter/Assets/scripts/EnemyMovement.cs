cab06f21ae73a441166b9d24000a722133067276
Christian <c.t.segumalian@gmail.com>
2018-11-06 20:11:14 -0800

    Enemy collision and movement set. Need Dynamic RigidBody though. GLITCH: Player must be Kinematic for now, or strange rotations happen when it collides with enemies. This also means that it ignores Static RBs and shoves Dynamic RBs away.

--- 2DTopDownShooter/Assets/scripts/EnemyMovement.cs
+++ 2DTopDownShooter/Assets/scripts/EnemyMovement.cs
@@ -9,14 +9,31 @@ public class EnemyMovement : MonoBehaviour {
 	private Transform target;
 
     public float speed;
-    public float attackRange;
     public float waitTime;
     public float startWaitTime;
 
-    public bool chasing;
+    //States representing what type of movement Enemy should be executing right now
+    public enum State {chase, attack, die};
+    public State currentAction = State.chase;
+
+    //Physics stuff relevant to movement
+    private Rigidbody2D rb;
+    private Collider2D thisCollider;
+
+    [Header("Attack")]
+    private float attackProgress = 0;
+    public Sprite defaultSprite;
+    public Sprite attackSprite;
+    private SpriteRenderer sr;
+    public float attackRange;
+    public float attackTime;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentsInChildren<SpriteRenderer>()[0];
+        thisCollider = GetComponent<Collider2D>();
+
         waitTime = startWaitTime;
 		//Looks for the first GameObject tagged as "Player" and sets that GO's transform as the player attribute.
 		//SO DON'T HAVE ANYTHING OTHER THAN THE PLAYER TAGGED AS "Player"
@@ -27,20 +44,75 @@ public class EnemyMovement : MonoBehaviour {
 
     void Update()
     {
-        Look(target);
-
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        if(Vector2.Distance(transform.position, target.position) <= attackRange)
+        switch(currentAction)
         {
-            //Debug.Log("attack");
+            case State.chase:
+            {
+                Move();
+                break;
+            }
+            case State.attack:
+            {        
+                ExecuteAttack();
+                break;
+            }
+            case State.die:
+            {
+                break;
+            }
+            default:
+                break;
         }
+    }
+
+    private void Move()
+    {
+        //Face the target node/player
+        Look(target);
+        //Move rigidbody and enemy position towards the target, smoothing any collisions with other objects.
+        rb.MovePosition(Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime));
+
+        //Start attacking if player is close enough
+        if(Vector2.Distance(transform.position, playerTransform.position) <= attackRange)
+        {
+            currentAction = State.attack;
+            sr.sprite = attackSprite;
+        }
+        
     }
 
     private void Look(Transform toLook)
     {
+        /*This was given by the Scaffold.
+        */
         Vector3 dir = toLook.position - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void ExecuteAttack()
+    {
+        /* Zero Out Movement
+        For some reason, the enemy will move oddly without these
+        such as spiraling away from the player.
+        This also makes sure that if another enemy is trying to shove its
+        way to the player, the enemies that are being shoved won't spiral away
+        from the player.*/
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        
+        //Look(target);
+        
+        //Track how long the enemy has been attacking.
+        //If it's longer than or equal the time it should
+        //take to attack, stop and go back to chasing the player.
+        attackProgress += Time.deltaTime;
+        if(attackProgress >= attackTime)
+        {
+            attackProgress = 0;
+            sr.sprite = defaultSprite;
+            currentAction = State.chase;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
