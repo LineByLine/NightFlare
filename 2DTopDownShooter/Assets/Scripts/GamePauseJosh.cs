9e465cb208318e95c5b645c1ae7c0d7d4f925a6f
Syamur <32967783+Syamur@users.noreply.github.com>
2018-11-07 18:51:50 -0800

    returned game project files; safe for merge

---
+++ 2DTopDownShooter/Assets/Scripts/GamePauseJosh.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePauseJosh : MonoBehaviour {
+    private bool gamePaused = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape") || Input.GetKeyDown("p"))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+	}
+    private void Resume()
+    {
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+    private void Paused()
+    {
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+}
