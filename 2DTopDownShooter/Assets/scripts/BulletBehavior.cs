4d668577ccdcb6ffe27633a54f9429a651ed4835
Christian <c.t.segumalian@gmail.com>
2018-11-10 12:35:54 -0800

    Infinite ammo, reload and fire cooldowns
    GLITCH: Sometimes bullets clip through walls and I don't know why.

--- 2DTopDownShooter/Assets/scripts/BulletBehavior.cs
+++ 2DTopDownShooter/Assets/scripts/BulletBehavior.cs
@@ -6,15 +6,17 @@ public class BulletBehavior : MonoBehaviour {
 	private SpriteRenderer sr;
 	private Rigidbody2D rb;
 	public float speed;
+	public float maxDistance;
 	private enemyBehavior damaging; //the enemy script to access variables
+	private float distTravelled;
 
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("Locked and loaded");
 		sr = gameObject.GetComponent<SpriteRenderer>();
 		rb = gameObject.GetComponent<Rigidbody2D>();
 		gameObject.transform.rotation = GameObject.FindGameObjectsWithTag("Player")[0].transform.rotation;
+		distTravelled = 0;
 
 	}
 	
@@ -23,8 +25,13 @@ public class BulletBehavior : MonoBehaviour {
 		//WHY DOES THIS WORK
 		//I couldn't figure out how to add the correct velocity, so I added the current position to a vector
 		//Why does this vector make it go in the direction I want?
-		rb.MovePosition(Vector2.MoveTowards(transform.position, transform.position + new Vector3(speed * Time.deltaTime, 0.0f, 0.0f), speed * Time.deltaTime));
+		//rb.MovePosition(Vector2.MoveTowards(transform.position, transform.position + new Vector3(speed * Time.deltaTime, 0.0f, 0.0f), speed * Time.deltaTime));
 		gameObject.transform.Translate(new Vector2(speed * Time.deltaTime, 0.0f));
+		distTravelled += speed * Time.deltaTime;
+		if(distTravelled >= maxDistance)
+		{
+			Destroy(gameObject);
+		}
 		
 	}
 
@@ -33,6 +40,7 @@ public class BulletBehavior : MonoBehaviour {
 		string tag = collision.gameObject.tag;
 		if (tag != "Player")
 		{
+			Debug.Log(tag);
 			if (tag == "Enemy")
 			{
 				damaging = collision.gameObject.GetComponent<enemyBehavior>();
--- 2DTopDownShooter/Assets/scripts/playerShootingProjectile.cs
+++ 2DTopDownShooter/Assets/scripts/playerShootingProjectile.cs
@@ -4,39 +4,59 @@ using UnityEngine;
 
 public class playerShootingProjectile : MonoBehaviour {
 	public GameObject bulletObject;
+	public float fireCooldown;
+	public float reloadTime;
 	[Header("Shooting Number Variables")]
 	public Transform firingPoint; //Where the bullet will travel/check from (realistic gun)
 	private weaponInfo useClip; //the weapon info script to access variables
-
+	private float currentFireCooldown;
+	private float currentReloadProgress;
 	// Use this for initialization
 	void Start () {
-
+		currentFireCooldown = fireCooldown;
+		currentReloadProgress = 0;
         useClip = firingPoint.GetComponent<weaponInfo>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1"))
+		if (Input.GetButton("Fire1"))
 		{
 			fire();
+		}
+		else if(Input.GetButtonUp("Fire1"))//If we want to let the player mash the Fire button to fire faster
+		{
+			useClip.reload();
+			currentFireCooldown = fireCooldown;
 		}
 	}
 
 	void fire()
 	{
 		//if your current ammo is empty, we try to reload
-		if (useClip.currentClip == 0)
+		if (useClip.currentClip == 0 && currentReloadProgress >= reloadTime)
 		{
+			currentReloadProgress = 0;
 			useClip.reload();
 		}
-		else //you can fire if u have ammo in your clip
-		{		
+		else if(useClip.currentClip == 0)
+		{
+			currentReloadProgress += Time.deltaTime;
+		}
+		else if (currentFireCooldown >= fireCooldown)//you can fire if u have ammo in your clip and fireCooldown has worn off
+		{
 			//Spawn bullet
 			GameObject.Instantiate(bulletObject, gameObject.transform.position,  gameObject.transform.rotation);
 			
-
 			//fires one bullet, subtracting from the current ammo 
 			useClip.currentClip--;
+			
+			//Start FireCooldown
+			currentFireCooldown = 0;
+		}
+		else//need to wait for cooldown
+		{
+			currentFireCooldown += Time.deltaTime;
 		}
 	}
 }
