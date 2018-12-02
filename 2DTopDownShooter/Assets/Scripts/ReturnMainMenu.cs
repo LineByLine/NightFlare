22683d014b72e1582cdd8cd46c2461c7d9ee0343
rhartant@uci.edu <44073902+rhartant@users.noreply.github.com>
2018-12-01 17:29:54 -0800

    Can access credits screen from main menu

---
+++ 2DTopDownShooter/Assets/Scripts/ReturnMainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnMainMenu : MonoBehaviour {
+
+    public GameObject MainMenuContainer;
+    public GameObject CreditsContainer;
+
+    public void returnToMenu()
+    {
+        MainMenuContainer.SetActive(true);
+        CreditsContainer.SetActive(false);
+    }
+}
